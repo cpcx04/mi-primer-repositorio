8c8cb1fe68b63ced662bd9cd23ce8cae145842b9
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-01-17 09:32:17 +0100

    Arreglao

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class Holamundo{
     public static void main(String[]){
         System.out.println("Hola mundo");
 
-        for (;;){
+        for (i=0;i<10;i++){
             System.out.println("Hola mundo");
         }
     }
