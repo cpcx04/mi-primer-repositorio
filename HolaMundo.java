5f23aa12ebb46acbfd3ee4454f8be662b9e49561
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-01-17 09:23:18 +0100

    Hola mundo terminado así

---
+++ HolaMundo.java
@@ -0,0 +1,5 @@
+public class Holamundo{
+    public static void main([]String){
+        System.out.println("Hola mundo");
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# mi-primer-repositorio
-Este es mi primer repositorio de GitHub, soy Cristian Pulido Cabello
